b2dc6f47a9771986881a53f1b0a29c884e2462b7
Zach Daniel <zach@zachdaniel.dev>
2025-04-15 15:05:41 -0400

    chore: release version v0.2.4

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule AshOps.MixProject do
   use Mix.Project
 
   @moduledoc "An Ash extension which generates mix tasks for Ash actions"
-  @version "0.2.3"
+  @version "0.2.4"
   def project do
     [
       aliases: aliases(),
